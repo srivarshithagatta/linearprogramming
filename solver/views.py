b7110f94348bf8286f80745656e9087ee1ad1a5c
srivarshithagatta <srivarshithagatta@gmail.com>
2025-02-03 11:31:53 +0530

    User Interface

---
+++ solver/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    
+    return render(request, 'home.html',{'name':"raju"})
+def solve(request):
+    return render(request, 'solve.html')
